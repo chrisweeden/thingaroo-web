08b6e1d2f960805ebe33182b6c6c5d63ce49d4aa
chrisweeden <chris.weeden@gmail.com>
2017-09-09 16:51:13 -0400

    Created a Thing model with TimestampedModel to provide timestamps fields

--- thingaroo/things/models.py
+++ thingaroo/things/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class TimestampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        abstract=True
+
+class Thing(TimestampedModel):
+    name = models.CharField(max_length=255)
